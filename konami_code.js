462c02baa3d0a71368b4f62dcffe3bc88f0110b5
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-01-31 23:06:49 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,23 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let tracker = 0 
+
+const checkKeyCode = (event) => {
+  const whatTheyPressed = event.keyCode
+  if (whatTheyPressed === code[tracker]) {
+    tracker += 1 
+  } else {
+    tracker = 0
+  }
+  console.log(tracker)
+}
+
 
 function init() {
   // Write your JavaScript code inside the init() function
-
+  // keydown
+  // 1 - find an element ? no - we only need the document
+  // 2 - make the function to trigger 
+  document.addEventListener('keydown', checkKeyCode)
 }
+
+init()
